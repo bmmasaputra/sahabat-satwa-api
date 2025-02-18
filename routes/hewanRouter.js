495df363876640da73fa379ff624b26285620320
bmmasaputra <bimagung2203@gmail.com>
2025-02-18 15:56:13 +0700

    fix: typos

--- routes/hewanRouter.js
+++ routes/hewanRouter.js
@@ -3,7 +3,7 @@ import {
   addNewHewan,
   getHewanByPawrent,
   updateHewanData,
-  deletetHewanById,
+  deleteHewanById,
 } from "../controller/hewanController.js";
 
 const router = express.Router();
@@ -11,6 +11,6 @@ const router = express.Router();
 router.get("/hewan", getHewanByPawrent);
 router.post("/hewan", addNewHewan);
 router.put("/hewan", updateHewanData);
-router.delete("/hewan", deletetHewanById);
+router.delete("/hewan", deleteHewanById);
 
 export default router;
